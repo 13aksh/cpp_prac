cf221115ee45e036832308fccf9e852ab7cb6034
13aksh <1392aksh@gmail.com>
2016-08-20 23:28:18 +0530

    For Loop

---
+++ for_loop.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a,b,i;
+    scanf("%d %d",&a,&b);
+    for(i=a;i<=b;i++){
+        if(i == 1) cout<<"one"<<endl;
+        if(i == 2) cout<<"two"<<endl;
+        if(i == 3) cout<<"three"<<endl;
+        if(i == 4) cout<<"four"<<endl;
+        if(i == 5) cout<<"five"<<endl;
+        if(i == 6) cout<<"six"<<endl;
+        if(i == 7) cout<<"seven"<<endl;
+        if(i == 8) cout<<"eight"<<endl;
+        if(i == 9) cout<<"nine"<<endl;
+        if (i > 9) {
+            if(i%2 == 0) cout<<"even"<<endl;
+            else cout<<"odd"<<endl;
+        }
+    }
+    return 0;
+}
